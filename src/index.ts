d3f3d5380948b429a134d780a6a295b2314aa69f
youtapong <youtapong@gmail.com>
2026-08-09 18:14:15 +0700

    feat: install swagger plugin and configure base path prefix to /fengsense

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const app = new Elysia({ prefix: '/fengsense' })
+  .use(swagger())
+  .get("/", () => "Hello Elysia")
+  .listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
